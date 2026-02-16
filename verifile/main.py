9e8387ffb97fb89f09d79ddf4a3c9cb8a97d0b10
Kevin <kevinjosecheriachery@gmail.com>
2026-02-16 12:56:24 +1100

    Feat: Reporter implemented

--- verifile/main.py
+++ verifile/main.py
@@ -4,6 +4,7 @@ import os
 
 from verifile.hasher import calculate_hash256
 from verifile.detector import detect_file_type
+from verifile.reporter import print_report
 
 def main():
     parser = argparse.ArgumentParser(
@@ -19,22 +20,38 @@ def main():
     filepath = args.filepath
     print(f"Identifying and verifying file {filepath}...")
     
-    if not os.path.exists(filepath):
-        print(f"Error: File does not exist.")
+    if not os.path.isfile(filepath):
+        print("Error: Invalid file path.")
         sys.exit(1)
+
     
     if not os.path.isfile(filepath):
         print(f"Error: Path is not a file.")
         sys.exit(1)
         
-    detector_result = detect_file_type(filepath, as_mime=True)
-    print(f"Detected file type: {detector_result}")
+    detector_result = detect_file_type(filepath, as_mime=True)    
+    extension = os.path.splitext(filepath)[1].lower()
     
-    extention = os.path.splitext(filepath)[1].lower()
-    print(f"File extension: {extention}")
+    mismatch = False
+    if extension:
+        ext_clean = extension.replace(".", "")
+    if "/" in detector_result:
+        mime_subtype = detector_result.split("/")[-1].lower()
+        if ext_clean != mime_subtype:
+            mismatch = True
     
     hashed_file = calculate_hash256(filepath)
-    print(f"SHA-256 hash: {hashed_file}")
+    filename = os.path.basename(filepath)
+    filesize = os.path.getsize(filepath)
+    print_report(
+    filename=filename,
+    filesize=filesize,
+    extension=extension,
+    filetype=detector_result,
+    filehash=hashed_file,
+    mismatch=mismatch
+)
+
 
 if __name__ == "__main__":
     main()
---
+++ verifile/reporter.py
@@ -0,0 +1,17 @@
+def print_report(filename: str, filesize: int, extension: str, filetype: str, filehash: str, mismatch: bool) -> None:
+    print("\n" + "=" * 50)
+    print("VeriFile Analysis Report")
+    print("=" * 50)
+    print(f"File name:{filename} ")
+    print(f"File size: {filesize}")
+    print(f"Extension: {extension}")
+    print(f"Type: {filetype}")
+    print(f"Hash: {filehash}")
+    
+    if mismatch:
+        print(f"Signature: ❌ Mismatch Detected")
+    else:
+        print(f"Signature: ✅ Match Detected")
+    print("=" * 50 + "\n")
+    
+    
